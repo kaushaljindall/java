9704a71b8628d0e6c099b681c9b461c248d2a51a
kaushaljindall <kaushaljindal14@gmail.com>
2026-07-22 09:31:21 +0530

    datatype

---
+++ chapter2/casting.java
@@ -0,0 +1,49 @@
+package chapter2;
+
+public class casting {
+    public static void main(String[] args) {
+        // Implicit COnversion --> destination var should be have more width than source var 
+        
+        
+        // (byte to int)
+        byte i = 10; 
+        int x; 
+        x = i ;
+
+
+        // (Char to int )
+        char c = 'a';
+        int e;
+        e = c;
+
+
+        System.out.println(x);
+        System.out.println(e);
+
+
+        //explicit Convertion
+
+        int b = 24;
+        byte t; 
+        t = (byte)b;
+        System.out.println(t);
+
+
+
+        // CRAZY CASE 
+        int r = 300;  
+        byte m; 
+        m = (byte)r;  
+        System.out.println(m);
+        //byte can store upto 8 bits (RANGE : -128 to 127)
+
+        // Truncating Conversion 
+        
+        float f = 89.42f ;
+        int u ;
+        u = (int)f; // 89 
+
+        System.out.println(u);
+
+    }
+}
---
+++ chapter2/datatypes.java
@@ -0,0 +1,24 @@
+package chapter2;
+
+public class datatypes {
+    public static void main(String[] args) {
+        // Intergers
+
+        byte b = 5;
+        short c = 10; 
+        int i = 2000;
+        long l = 12494;
+        
+
+
+        // Real Numbers
+
+        float pi = 3.1415f;
+        double pii = 3.1415;
+
+
+        System.out.println("Integer Values: " + b + ", "  + c +", " + i + ", " + l);
+        System.out.println("Real Values: " + pi + ", "  + pii);
+
+    }
+}
